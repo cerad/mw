667f84918483ce84b7da591d220f2d29d567aa10
Artx Hundiak <ahundiak@gmail.com>
2015-07-09 17:35:36 -0500

    New React Component Tests

--- tests/React/TitleTestComponent.php
+++ tests/React/TitleTestComponent.php
@@ -5,6 +5,8 @@ class TitleTestComponent extends Component2
 {
   public function render()
   {
-    return sprintf('<title>%s</title>',$this->escape($this->props['title']));
+    return <<<TYPEOTHER
+<title>{$this->escape($this->props['title'])}</title>
+TYPEOTHER;
   }
 }
